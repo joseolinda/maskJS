6a1f7740e4c97e2b2987beffcff1750e37ef9a1d
José Olinda <joseolindad@yahoo.com.br>
2019-08-22 08:56:27 -0300

    Add files via upload

--- js/vendor/MaskJS.js
+++ js/vendor/MaskJS.js
@@ -8,5 +8,31 @@
         }
     }
 )( this, function () {
+    const   NUMERO = "9",
+            LETRA = "A",
+            LETRA_E_NUMERO = "S",
+            TECLAS_PROIBIDAS = [9, 16, 17, 18, 36, 37, 38, 39, 40, 91, 92, 93],
+            teclaPermitida = function (codigoTecla) {
+                for( let i = 0; i < TECLAS_PROIBIDAS.length; i++){
+                    if ( codigoTecla === TECLAS_PROIBIDAS[i]) {
+                        return false;
+                    }
+                    return true;
+                }
+            },
+        mesclarOpcoesMoeda = function(opcoes) {
+            opcoes = opcoes || {};
+            opcoes = {
+                casasDecimais: opcoes.hasOwnProperty("casasDecimais") ? opcoes.casasDecimais : 2,
+                separadorDecimal: opcoes.separadorDecimal || ",",
+                separadorMilhar: opcoes.separadorMilhar || ".",
+                simboloMoeda: opcoes.simboloMoeda && (opcoes.simboloMoeda.replace(/[\s]/g,'') + " ") || "",
+                simboloMoedaNoFinal: opcoes.simboloMoedaNoFinal && (" " + opcoes.simboloMoedaNoFinal.replace(/[\s]/g,'')) || "",
+                zeroCentavos: opcoes.zeroCentavos,
+                saidaExibida: opcoes.saidaExibida
+            };
+            opcoes.precisao = opcoes.zeroCentavos ? 0 : opcoes.casasDecimais;
+            return opcoes;
+        },
     return alert();
 });
